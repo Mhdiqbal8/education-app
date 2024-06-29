46c8e9f8e1ef3b07bdddce0e7540bf110f746e00
mhdiqbal8 <muh.iqbal976@gmail.com>
2024-06-30 02:02:55 +0700

    upload sample education app

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+        onPress={() => {
+          console.log("You tapped the button!");
+        }}
+      >
+        <Text style={styles.text}>HELLO WORLDS</Text>
+      </TouchableOpacity>
+      <View style={styles.flex}>
+        <Text>1</Text>
+        <Text>2</Text>
+        <Text>3</Text>
+        <Text>4</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  flex: {
+    display: "flex",
+    gap: 10,
+    flexDirection: "row",
+  },
+  text: {
+    color: "blue",
+    fontSize: 40,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
